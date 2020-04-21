b5baf0cc4e22b96465a8714f19d67bbf5523dae6
ApichaJA <junyatanakorn@hotmail.com>
2020-04-22 01:07:59 +0700

    add todos and delete button

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,58 @@
 <template>
   <div id="app">
-    <Todos text_top="My to-dos"/>
+    <h1>My to-dos</h1>
+    <input v-on:keyup.enter="submit" v-model="eventInput" placeholder="Create a new to-do...">
+    <ul class="row">
+      <li v-for="Mytodos in eventTodos" :key="Mytodos">
+        <div class="myTodos">
+          <label>{{Mytodos.title}}</label>
+          <button class="edit">edit</button>
+          <button class="delete" @click="deleteTodos(Mytodos)">delete</button>
+        </div>
+      </li>
+    </ul>
+    <MyTodos/>
   </div>
 </template>
 
 <script>
-import Todos from './components/MyTodos.vue'
 
 export default {
   name: 'app',
-  components: {
-    Todos
-  }
+  data(){
+    return{
+      eventInput:"",
+      eventTodos:[
+        {id: 0, title: 'Do the dished'},
+        {id: 0, title: 'Take out the trash'},
+        {id: 0, title: 'Finish doing laundry'},
+      ]
+    }
+  },
+  methods: {
+    submit(){
+      this.eventTodos.push({
+        title: this.eventInput,
+        id: 1
+      })
+    this.eventInput = ""
+    },
+    deleteTodos(Mytodos){
+      this.eventTodos.splice(this.eventTodos.indexOf(Mytodos), 1);
+    }
+  },
 }
 </script>
 
 <style>
 body {
   margin: 0;
+  box-sizing: border-box;
+}
+.myTodos{
+  display: flex!important;
+  padding: .75rem 1.25rem;
+  
 }
 #app {
   font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",
--- src/components/MyTodos.vue
+++ src/components/MyTodos.vue
@@ -1,19 +1,9 @@
 <template>
-  <div class="My-to-dos">
-    <h1>{{ text_top }}</h1>
-    <input v-model="message" placeholder="edit me pls">
-  </div>
+<div>
+</div>
 </template>
 
 <script>
   export default {
-    name: 'Todos',
-    props: {
-      text_top: String,
-    }
   }
 </script>
-
-<style>
-
-</style>
